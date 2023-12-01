86e25ec962b20c79e0b50516e8e9bb13bb7ac9f3
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-01 08:10:57 -0300

    Criando entity Post com User aninhado

--- workshopmongo/src/main/java/com/anatpc/workshopmongo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/anatpc/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.anatpc.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.anatpc.workshopmongo.domain.User;
+import com.anatpc.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
